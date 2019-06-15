4430faee4ced698726acf35053cff68c58437af5
brankLY <1124185356@qq.com>
2019-06-15 14:26:22 +0800

    [刘阳] add method definition-instantiate

--- gameService.java
+++ gameService.java
@@ -1,3 +1,12 @@
 public interface gameService {
+
     boolean[][] init(int row, int col);
+
+    boolean [][]instantiate(int initNums[][]);
+
+    boolean [][]instantiate(int random);
+
+    boolean [][]instantiate(int chooseNumer);
+
 }
+
