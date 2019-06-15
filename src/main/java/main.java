e1b55338f764363f68b72b570569bdc447f32d0c
brankLY <1124185356@qq.com>
2019-06-15 18:03:04 +0800

    remove somethings

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,17 +2,5 @@ import com.tw.example.service.gameServiceImplement;
 
 public class main {
 	public static void main(String[] args) {
-		gameServiceImplement service = new gameServiceImplement();
-		service.init(3,3);
-		int initNums[][] = {{1,2,3},{
-			0,1,1},{
-			1,0,1}};
-		service.instantiate(initNums);
-//		boolean [][]test = service.getMatrix();
-		for(int i=0;i<initNums.length;i++){
-			for (int j=0;j<initNums[0].length;j++){
-				System.out.println(initNums[i][j]);
-			}
-		}
 	}
 }
