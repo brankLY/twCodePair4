3f4edb7356e42ac696b5fffa9827723a15253396
brankLY <1124185356@qq.com>
2019-06-15 15:41:20 +0800

    [刘阳] add implement instantiate method

--- src/main/gameServiceImplement.java
+++ src/main/gameServiceImplement.java
@@ -5,13 +5,45 @@ public class gameServiceImplement implements gameService{
 
     public boolean[][] init(int row, int col){
       this.matrix = new boolean[row][col];
+      return matrix;
     }
 
-    boolean[][] instantiate(int initNums[][]);
+    // 根据初始二维矩阵实例化
+    // 初始矩阵 eg（3*3）：
+    // 1 0 1
+    // 0 1 1
+    // 1 0 0
+    // 矩阵值为1，实例化matrix值为true
+    // 矩阵值为其他，实例化matrix值为false
+    public boolean[][] instantiate(int initNums[][]){
+        if(initNums == null || initNums[0] == null){
+            return null;
+        }
+        for(int i=0;i<initNums.length;i++){
+            for (int j=0;j<initNums[0].length;j++){
+                if(initNums[i][j] == 1){
+                    matrix[i][j] = true;
+                }else {
+                    matrix[i][j] = false;
+                }
 
-    boolean[][] instantiateByRandom(int random);
+            }
+        }
+        return matrix;
+    }
 
-    boolean[][] instantiateByChoose(int chooseNumer);
+    public boolean[][] instantiateByRandom(int random){
+        if(random<0 || random>=100){
 
-    boolean[][] next();
+        }
+        return matrix;
+    }
+
+    public boolean[][] instantiateByChoose(int chooseNumer){
+        return matrix;
+    }
+
+    public boolean[][] next(){
+        return matrix;
+    }
 }
